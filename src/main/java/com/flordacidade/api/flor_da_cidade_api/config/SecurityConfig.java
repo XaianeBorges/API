2a8845716ea03a700dcd5cb11dd1034050b8a4ed
Xaiane Borges <xaianebezerra@gmail.com>
2025-09-02 19:18:03 -0300

    ajuste na permissão de areas classificacao

--- src/main/java/com/flordacidade/api/flor_da_cidade_api/config/SecurityConfig.java
+++ src/main/java/com/flordacidade/api/flor_da_cidade_api/config/SecurityConfig.java
@@ -66,6 +66,7 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.GET, "/api/tipos-horta/**").permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/unidades-ensino/**").permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/atividades-produtivas/**").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/areas-classificacao/**").permitAll()
 
                         // Permissões para fluxos de criação pública
                         .requestMatchers(HttpMethod.POST, "/api/pessoas").permitAll()
