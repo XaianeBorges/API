68cc7794fa78b28692d929925e3000e65be8723a
Xaiane Borges <xaianebezerra@gmail.com>
2025-09-02 21:22:42 -0300

    Ajuste para VM

--- src/main/java/com/flordacidade/api/flor_da_cidade_api/config/SecurityConfig.java
+++ src/main/java/com/flordacidade/api/flor_da_cidade_api/config/SecurityConfig.java
@@ -89,7 +89,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(Arrays.asList(
-                "http://localhost:5173", "http://localhost:5175", "http://localhost:5174"));
+                "http://localhost:5173", "http://localhost:5175", "http://localhost:5174", "http://flordacidade.ddns.net"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
